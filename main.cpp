e5cf2edf53c0c2f9e74f8310df54767c5634feb5
Lyess10 <148151716+Lyess10@users.noreply.github.com>
2025-12-04 09:56:11 +0100

    Refactor main function and add NPI expression reader

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <stack>
+#include <string>
 
 using namespace std;
 
@@ -7,49 +9,92 @@ using namespace std;
 #include "Soustraction.h"
 #include "Division.h"
 #include "Multiplication.h"
+#include "Expression.h"
 
-int main(void) {
-    Constante c1(20.0);
+// Fonction utilitaire : construit une Expression* a partir d une saisie NPI
+Expression* lire_expression_npi() {
+    stack<Expression*> pile;
+    string token;
 
-    c1.afficher_classique();
+    cout << "=== Calculatrice NPI (notation polonaise inversee) ===" << endl;
+    cout << "Entrez votre expression, un element a la fois," << endl;
+    cout << "puis 'p' pour terminer et calculer." << endl;
+    cout << endl;
+    cout << "Exemple pour (5/2 + 4) * (-20 - 3) :" << endl;
+    cout << "  5 2 / 4 + -20 -3 + * p" << endl << endl;
+
+    while (cin >> token && token != "p") {
+
+        // Est-ce un operateur ? (un seul caractere parmi + - * /)
+        if (token.size() == 1 &&
+            (token[0] == '+' || token[0] == '-' || token[0] == '*' || token[0] == '/')) {
+
+            if (pile.size() < 2) {
+                cerr << "Erreur : pas assez d operandes sur la pile pour appliquer l operateur '"
+                     << token << "'." << endl;
+                return nullptr;
+            }
+
+            // On depile d abord l operande droit puis l operande gauche
+            Expression* opd = pile.top(); pile.pop();
+            Expression* opg = pile.top(); pile.pop();
+
+            Expression* expr = nullptr;
+            switch (token[0]) {
+                case '+':
+                    expr = new Addition(opg, opd);
+                    break;
+                case '-':
+                    expr = new Soustraction(opg, opd);
+                    break;
+                case '*':
+                    expr = new Multiplication(opg, opd);
+                    break;
+                case '/':
+                    expr = new Division(opg, opd);
+                    break;
+            }
+
+            pile.push(expr);
+        }
+        else {
+            // Sinon, on considere que c est un nombre (entier ou flottant, eventuellement negatif)
+            float valeur = stof(token);  // on suppose la syntaxe correcte
+            pile.push(new Constante(valeur));
+        }
+    }
+
+    if (pile.size() != 1) {
+        cerr << "Erreur : expression incomplete ou mal formee (pile.size = "
+             << pile.size() << ")." << endl;
+        return nullptr;
+    }
+
+    return pile.top();
+}
+
+int main() {
+    Expression* expr = lire_expression_npi();
+    if (!expr) {
+        return 1;  // erreur deja affichee
+    }
+
+    cout << endl;
+    cout << "=== Resultat ===" << endl;
+
+    cout << "Notation classique : ";
+    expr->afficher_classique(cout);
     cout << endl;
 
-    Constante c2(30.0);
-    Addition add1(&c1, &c2);
-
-    add1.afficher_classique(cout);
+    cout << "Notation polonaise inversee : ";
+    expr->afficher_npi(cout);
     cout << endl;
 
-    add1.afficher_npi(cout);
-    cout << endl;
+    cout << "Valeur numerique : " << expr->calculer() << endl;
 
-    Constante c3(34.0);
-    Multiplication m1(&c3, &add1);
-
-    m1.afficher_classique(cout);
-    cout << endl;
-
-    m1.afficher_npi(cout);
-    cout << endl;
-
-    Constante c4(50.0);
-    Constante c5(10.0);
-    Soustraction s1(&c4, &c5);
-
-    s1.afficher_classique(cout);
-    cout << endl;
-
-    s1.afficher_npi(cout);
-    cout << endl;
-
-    Division d1(&m1, &c5);
-
-    d1.afficher_classique(cout);
-    cout << endl;
-
-    d1.afficher_npi(cout);
-    cout << endl;
+    // Si le destructeur d OperateurBinaire detruit ses operandes,
+    // un seul delete ici suffit pour tout liberer
+    delete expr;
 
     return 0;
 }
-
