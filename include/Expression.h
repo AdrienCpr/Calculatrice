e0676ea85dde2cf0ec4c7cae93d483a55c1badd7
MerwanVid <merwan.vidal@gmail.com>
2025-12-04 11:33:10 +0100

    Add NPI save/load functions to Expression.h
    Added functions for saving and loading expressions in NPI format.

--- include/Expression.h
+++ include/Expression.h
@@ -2,6 +2,7 @@
 #define EXPRESSION_H
 
 #include <iostream>
+#include <string>
 using namespace std;
 
 class Expression
@@ -13,10 +14,17 @@ class Expression
         virtual void afficher_classique(ostream &os = cout) = 0;
         virtual void afficher_npi(ostream &os = cout) = 0;
         virtual float calculer() = 0;
-
-    protected:
-
-    private:
 };
+
+// ---------- Fonctions de sauvegarde / chargement en NPI ----------
+
+// Sauvegarde l'expression en NPI dans un fichier texte
+void sauvegarder_npi(Expression &expr, const std::string &nomFichier);
+
+// Charge une expression en NPI depuis un flux (ifstream, stringstream, ...)
+Expression* charger_npi(std::istream &is);
+
+// Charge une expression en NPI depuis un fichier texte
+Expression* charger_npi_fichier(const std::string &nomFichier);
 
 #endif // EXPRESSION_H
