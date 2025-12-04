0642bc9cb437613c401ba9af3b9ea70ab1c559fc
Lyess10 <148151716+Lyess10@users.noreply.github.com>
2025-12-04 09:55:07 +0100

    Fix division by zero error message
    Corrected error message for division by zero.

--- src/Division.cpp
+++ src/Division.cpp
@@ -1,4 +1,7 @@
 #include "Division.h"
+#include <iostream>    // pour cerr
+
+using namespace std;
 
 Division::Division(Expression *opg, Expression *opd) : OperateurBinaire(opg, opd)
 {
@@ -6,9 +9,8 @@ Division::Division(Expression *opg, Expression *opd) : OperateurBinaire(opg, opd
 
 Division::~Division()
 {
-    //dtor
+    // dtor
 }
-
 
 void Division::afficher_classique(ostream &os) {
     GetOperandeGauche()->afficher_classique(os);
@@ -17,18 +19,18 @@ void Division::afficher_classique(ostream &os) {
 }
 
 void Division::afficher_npi(ostream &os) {
+    // Meme logique que Addition / Soustraction / Multiplication
     GetOperandeGauche()->afficher_classique(os);
     os << " ";
     GetOperandeDroit()->afficher_classique(os);
     os << " /";
 }
 
-
 float Division::calculer() {
     float denominateur = GetOperandeDroit()->calculer();
 
     if (denominateur == 0) {
-        cerr << "Erreur : division par zéro !" << endl;
+        cerr << "Erreur : division par zero !" << endl;
         return 0;
     }
 
