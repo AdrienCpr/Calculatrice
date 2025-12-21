1813023ed53b8dc8bc43407f4b85a74911908dd3
Guiquent16 <guillaume.vial.2003@gmail.com>
2025-12-21 21:21:55 +0100

    Update Addition.cpp

--- src/Addition.cpp
+++ src/Addition.cpp
@@ -2,7 +2,7 @@
 
 Addition::Addition(Expression *opg, Expression *opd) : OperateurBinaire(opg, opd)
 {
-    // à compléter
+
 }
 
 Addition::~Addition()
@@ -10,17 +10,21 @@ Addition::~Addition()
     //dtor
 }
 
+
 void Addition::afficher_classique(ostream &os) {
-    GetOperandeGauche()->afficher_classique(os);
-    os << " + ";
-    GetOperandeDroit()->afficher_classique(os);
+    cout << "( ";
+    GetOperandeGauche()->afficher_classique();
+    cout << " + ";
+    GetOperandeDroit()->afficher_classique();
+    cout << " )";
 }
 
 void Addition::afficher_npi(ostream &os) {
-    GetOperandeGauche()->afficher_classique(os);
-    os << " ";
-    GetOperandeDroit()->afficher_classique(os);
-    os << " +";
+    cout << "\n ";
+    GetOperandeGauche()->afficher_npi();
+    cout << " ";
+    GetOperandeDroit()->afficher_npi();
+    cout << " +\n";
 }
 
 float Addition::calculer() {
